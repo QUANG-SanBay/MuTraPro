3f6e9293115575f6186edda578e41c363237ded8
QUANG-SanBay <quangsanbay@gmail.com>
2025-09-26 16:13:06 +0700

    setup project MuTraPro

---
+++ backEnd/src/main/java/com/mutrapro/mutrapro/MutraproApplication.java
@@ -0,0 +1,13 @@
+package com.mutrapro.mutrapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutraproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutraproApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/mutrapro/mutrapro/MutraproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutrapro.mutrapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutraproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
